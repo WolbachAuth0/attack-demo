d4fc27ace7d07c681cad24ac22b8370412f9ddce
WolbachAuth0 <aaron.wolbach@okta.com>
2022-10-11 08:28:51 -0700

    refactor for readability

--- src/main.js
+++ src/main.js
@@ -12,56 +12,17 @@ main()
 
 async function main () {
   try {
-    const environment = await inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'name',
-          message: 'Select the Auth0 tenant:',
-          choices: Tenant.directories.map(tenant => tenant.name),
-        },
-      ])
-    
-    console.log(`\nLoading environment variables for the ${chalk.bold.green(environment.name)} tenant ...`)
-    const tenant = new Tenant({ scopes, name: environment.name })
+    // prompt user to select the tenant to attack
+    const tenant = await chooseTenant()
     const clients = await tenant.getClients()
-    
-    const application = await inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'name',
-          message: 'Select the Application to attack:',
-          choices: clients
-        },
-      ])
 
+    // prompt user to select which SPA, Web App or Mobile app to attack
+    const application = await chooseApplication(clients)
     const client = clients.find(x => x.name == application.name)
-    const connections = await tenant.getConnectionsForClient(client.client_id)
-    let connection = {}
+
+    // prompt user to select which connection to attack
+    const connection = await chooseConnection(tenant, client)
     
-    if (connections.length == 0) {
-      console.log(`No DB connections were found for the ${chalk.bold.redBright(client.name)} application.`)
-      console.log(chalk.bold.bgRed.white(`No connection to attack. Exiting.`))
-      process.exit(1)
-    } else if (connections.length > 1) {
-      console.log(`More than one DB connection was found for the ${chalk.bold.green(client.name)} application.`)
-      const choice = await inquirer
-        .prompt([
-          {
-            type: 'list',
-            name: 'name',
-            message: 'Select the specific DB connection to attack:',
-            choices: connections
-          }
-        ])
-      connection = connections.find(x => x.name == choice.name)
-    } else {
-      connection = connections[0]
-    }
-
-    const attacker = new Attacker({ connection, tenant, client })
-
     const attackType = await inquirer
       .prompt([
         {
@@ -69,24 +30,121 @@ async function main () {
           name: 'name',
           message: 'Select type of attack to perform:',
           choices: [
+            { name: 'Brute Force' },
             { name: 'Credential Stuffing' },
             { name: 'Fraudulent Sign-ups' }
           ]
         },
       ])
     
-      if (attackType.name == 'Credential Stuffing') {
-        runCredentialStuffingAttack(attacker)
-      } else if (attackType.name == 'Fraudulent Sign-ups') {
-        runFraudulentSignupAttack(attacker)
-      }
+     // instantiate attacker
+    const attacker = new Attacker({ connection, tenant, client })
+
+    if (attackType.name == 'Brute Force') {
+      runBruteForceAttack(attacker)
+    } else if (attackType.name == 'Credential Stuffing') {
+      runCredentialStuffingAttack(attacker)
+    } else if (attackType.name == 'Fraudulent Sign-ups') {
+      runFraudulentSignupAttack(attacker)
+    }
 
   } catch (error) {
     handleError(error)
   }
 }
 
+async function chooseTenant () {
+  const environment = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'name',
+        message: 'Select the Auth0 tenant:',
+        choices: Tenant.directories.map(tenant => tenant.name),
+      },
+    ])
+  
+  console.log(`\nLoading environment variables for the ${chalk.bold.green(environment.name)} tenant ...`)
+  const tenant = new Tenant({ scopes, name: environment.name })
+  return tenant
+}
+
+async function chooseApplication (clients) {
+  const application = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'name',
+        message: 'Select the Application to attack:',
+        choices: clients
+      },
+    ])
+  return application
+}
+
+async function chooseConnection (tenant, client) {
+  const connections = await tenant.getConnectionsForClient(client.client_id)
+  let connection = {}
+  
+  if (connections.length == 0) {
+    console.log(`No DB connections were found for the ${chalk.bold.redBright(client.name)} application.`)
+    console.log(chalk.bold.bgRed.white(`No connection to attack. Exiting.`))
+    process.exit(1)
+  } else if (connections.length > 1) {
+    console.log(`More than one DB connection was found for the ${chalk.bold.green(client.name)} application.`)
+    const choice = await inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'name',
+          message: 'Select the specific DB connection to attack:',
+          choices: connections
+        }
+      ])
+    connection = connections.find(x => x.name == choice.name)
+  } else {
+    connection = connections[0]
+  }
+  // console.log(connection.options.passwordPolicy)
+  // console.log(connection.options.password_complexity_options)
+  return connection
+}
+
 async function runCredentialStuffingAttack (attacker) {
+  
+  const attempts = await inquirer
+    .prompt([
+      {
+        type: 'number',
+        name: 'name',
+        message: 'How many attempts to make? (integer between 1 and 25)',
+        default: 10,
+        validate: (input) => {
+          if (input < 1) {
+            return `${input} is less than 1. Please provide an integer between 1 and 25.`
+          } else if (input > 25) {
+            return `${input} is greater than 25. Please provide an integer between 1 and 25.`
+          } else if (!Number.isInteger(input)) {
+            return `${input} is not an integer. Please provide an integer between 1 and 25.`
+          } else {
+            return true
+          }
+        }
+      }
+    ])
+
+  console.log('\n', chalk.bold.underline.blueBright('Begin Auth0 Scripted Attack Attempt'), '\n')
+  console.log('Attack Type: ', chalk.bold.green('Credential Stuffing Attack'))
+  console.log('number of attempts: ', chalk.bold.green(attempts.name))
+  console.log('Auth0 tenant: ', chalk.bold.green(attacker.tenant.name))
+  console.log('application: ', chalk.bold.green(`${attacker.client.name} (id= ${attacker.client.client_id})`))
+  console.log('connection: ', chalk.bold.green(`${attacker.connection.name} (id= ${attacker.connection.id})`))
+  console.log('\n')
+
+  attacker.credentialStuffing({ attempts: attempts.name })
+}
+
+async function runBruteForceAttack (attacker) {
   const email = await inquirer
     .prompt([
       {
@@ -103,21 +161,20 @@ async function runCredentialStuffingAttack (attacker) {
       }
     ])
 
-
   const attempts = await inquirer
     .prompt([
       {
         type: 'number',
         name: 'name',
-        message: 'How many attempts to make? (integer between 1 and 100)',
+        message: 'How many attempts to make? (integer between 1 and 25)',
         default: 10,
         validate: (input) => {
           if (input < 1) {
-            return `${input} is less than 1. Please provide an integer between 1 and 100.`
-          } else if (input > 100) {
-            return `${input} is greater than 100. Please provide an integer between 1 and 100.`
+            return `${input} is less than 1. Please provide an integer between 1 and 25.`
+          } else if (input > 25) {
+            return `${input} is greater than 25. Please provide an integer between 1 and 25.`
           } else if (!Number.isInteger(input)) {
-            return `${input} is not an integer. Please provide an integer between 1 and 100.`
+            return `${input} is not an integer. Please provide an integer between 1 and 25.`
           } else {
             return true
           }
@@ -126,7 +183,7 @@ async function runCredentialStuffingAttack (attacker) {
     ])
 
   console.log('\n', chalk.bold.underline.blueBright('Begin Auth0 Scripted Attack Attempt'), '\n')
-  console.log('Attack Type: ', chalk.bold.green('Credential Stuffing Attack'))
+  console.log('Attack Type: ', chalk.bold.green('Brute Force Attack'))
   console.log('email address: ', chalk.bold.green(email.name))
   console.log('number of attempts: ', chalk.bold.green(attempts.name))
   console.log('Auth0 tenant: ', chalk.bold.green(attacker.tenant.name))
@@ -170,8 +227,6 @@ async function runFraudulentSignupAttack (attacker) {
 
   attacker.fraudulentSignups({ count: count.name })
 }
-
-
 
 function handleError (error) {
   console.log(chalk.bold.underline.bgRed.white(error.message))
